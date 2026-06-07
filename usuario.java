5e54ffb35a6b57a1cf085ddac056e7f392663fa5
LaryCala <145489536+LaryCala@users.noreply.github.com>
2026-06-07 17:11:59 -0300

    Create usuario.java

---
+++ usuario.java
@@ -0,0 +1,64 @@
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String tipo;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, String email,
+                   String senha, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String obterDados() {
+        return nome + " - " + email;
+    }
+}
